ed96f92e8f694ab1edea74bc066a6023209e0604
OracunPB <paubs1@hotmail.com>
2025-02-12 09:04:48 +0100

    Push Plantilla Inicial

---
+++ plantilla.sql
@@ -0,0 +1,34 @@
+/* *****************************************************
+  INSTITUT TIC de Barcelona
+    CFGS _____________________________
+    Mòdul: 0484 Bases de dades. 
+    AUTORS: _______________________________
+    DATA: ________________
+****************************************************** */
+
+-- Pregunta 1
+select 'No ho sé';
+
+-- Pregunta 2
+select 'No ho sé';
+
+-- Pregunta 3
+select 'No ho sé';
+
+-- Pregunta 4
+select 'No ho sé';
+
+-- Pregunta 5
+select 'No ho sé';
+
+-- Pregunta 6
+select 'No ho sé';
+
+-- Pregunta 7
+select 'No ho sé';
+
+-- Pregunta 8
+select 'No ho sé';
+
+-- Pregunta 9
+select 'No ho sé';
